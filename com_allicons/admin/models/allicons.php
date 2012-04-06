ab61800754b7974049624e4fd2bf64f6b3ee2d42
Royce Harding <royceclrw@gmail.com>
2012-04-06 14:22:13 -0400

    Fast forward push?

--- com_allicons/admin/models/allicons.php
+++ com_allicons/admin/models/allicons.php
@@ -5,7 +5,6 @@ defined('_JEXEC') or die('Restricted access');
 jimport('joomla.application.component.modellist');
 /**
  * AllIconsList Model
- *
  */ 
 class AllIconsModelAllIcons extends JModelList
 {
