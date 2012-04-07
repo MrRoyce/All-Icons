e1c6b2f4c8bad9211aea1997c50b9aa1b1cd63c5
Royce Harding <royceclrw@gmail.com>
2012-04-07 00:26:54 -0400

    Mostly installer/preflight - auto publish and position the module.

--- mod_allicons/installer.php
+++ mod_allicons/installer.php
@@ -2,7 +2,7 @@
 
 defined('_JEXEC') or die('Restricted Access');
 
-class com_AllIconsInstallerScript
+class MOD_ALLICONSInstallerScript
 {
 
 	
@@ -29,7 +29,7 @@ class com_AllIconsInstallerScript
 		$this->release = $parent->get("manifest")->version; 
  
 		if( version_compare( $jversion->getShortVersion(), $this->minimum_joomla_release, 'lt' ) ) {
-			Jerror::raiseWarning(null, JText::_('COM_ALLICONS_CANNOT_INSTALL') . $this->minimum_joomla_release);
+			Jerror::raiseWarning(null, JText::_('MOD_ALLICONS_CANNOT_INSTALL') . $this->minimum_joomla_release);
 			return false;
 		}  
 
@@ -38,14 +38,14 @@ class com_AllIconsInstallerScript
 			$oldRelease = $this->getParam('version');
 			$rel = $oldRelease . ' -> ' . $this->release;
 			if ( version_compare( $this->release, $oldRelease, 'le' ) ) {
-				Jerror::raiseWarning(null, JText::_('COM_ALLICONS_CANNOT_INSTALL_SEQUENCE') . $rel);
+				Jerror::raiseWarning(null, JText::_('MOD_ALLICONS_CANNOT_INSTALL_SEQUENCE') . $rel);
 				return false;
 			}
 		} else { 
 			$rel = $this->release; 
 		}
 		
-		$parent->set('message', JText::_('COM_ALLICONS_PREFLIGHT_' . strtoupper($type)) . ' ' . $rel);		
+		$parent->set('message', JText::_('MOD_ALLICONS_PREFLIGHT_' . strtoupper($type)) . ' ' . $rel);		
         
     }
 	
@@ -58,13 +58,13 @@ class com_AllIconsInstallerScript
     function install($parent)
     {
         $this->release = $parent->get("manifest")->version;
-		$parent->set('message', JText::_('COM_ALLICONS_INSTALL') . $this->release);
+		$parent->set('message', JText::_('MOD_ALLICONS_INSTALL') . $this->release);
     }
     
     function uninstall($parent)
     {
         $this->release = $parent->get("manifest")->version;
-		$parent->set('message', JText::_('COM_ALLICONS_UNINSTALL') . ' ' . $this->release);
+		$parent->set('message', JText::_('MOD_ALLICONS_UNINSTALL') . ' ' . $this->release);
         
     }
 	
@@ -73,7 +73,7 @@ class com_AllIconsInstallerScript
 	 */
 	function getParam($name) {
 		$db = JFactory::getDbo();
-		$db->setQuery('SELECT `manifest_cache` FROM `#__extensions` WHERE `name` = ' . $db->quote('com_allicons'));
+		$db->setQuery('SELECT `manifest_cache` FROM `#__extensions` WHERE `name` = ' . $db->quote('MOD_ALLICONS'));
 		$manifest = json_decode($db->loadResult(), true);
 		return $manifest[$name];
 	}
@@ -87,7 +87,7 @@ class com_AllIconsInstallerScript
     function update($parent)
     {
         $this->release = $parent->get("manifest")->version;
-		$parent->set('message', JText::_('COM_ALLICONS_UPDATE') . $this->release);
+		$parent->set('message', JText::_('MOD_ALLICONS_UPDATE') . $this->release);
         
     }
     
@@ -100,7 +100,55 @@ class com_AllIconsInstallerScript
     
     function postflight($type, $parent)
     {
-		$this->release = $parent->get("manifest")->version;
-        $parent->set('message', JText::_('COM_ALLICONS_POSTFLIGHT') . $this->release);
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(TRUE);
+		
+		// 1st get the module id
+		$query->select('id')
+			->from('#__modules')
+			->where('module = "mod_allicons"');
+		$db->setQuery($query);
+		$moduleid = $db->loadResult();
+		
+		if ($moduleid) {
+			// Now update the modules table to set the module publish state and position
+			$query = $db->getQuery(TRUE);
+			$query->update('#__modules')
+				->set('published = 1')
+				->set('position = "icon"')
+				->where('id = ' . (int) $moduleid);
+			$db->setQuery($query);
+			if ($db->query()) {
+				// now update the modules_menu to add to all pages (menuid = 0)
+				
+				// but 1st see if it already exists!  not sure why framework calls postflight 2x!!
+				$query = $db->getQuery(TRUE);
+				// was having problems using menuid, maybe because it was-- 0???
+				$query->select('moduleid')
+					->from('#__modules_menu')
+					->where('moduleid = ' . (int) $moduleid);
+				$db->setQuery($query);
+				$existingid = $db->loadResult();
+
+				if (!($existingid)) { 
+					// now insert the menu row!!
+					$query = $db->getQuery(TRUE);
+					$query->insert('#__modules_menu')
+						->columns('moduleid, menuid')
+						->values((int) $moduleid . ', ' . 0);
+					$db->setQuery($query);
+					if (!($db->query())) {
+						Jerror::raiseWarning(null, JText::_('MOD_ALLICONS_COULD_NOT_MODIFY_MENU_TABLE') . ' ' . $db->getErrorMsg());
+					}
+				}
+			} else {
+				Jerror::raiseWarning(null, JText::_('MOD_ALLICONS_COULD_NOT_MODIFY_MODULE') . ' ' . $db->getErrorMsg());
+			}
+			
+		$db->query();
+		} else {
+			Jerror::raiseWarning(null, JText::_('MOD_ALLICONS_COULD_NOT_FIND_MODULE') . ' ' . $db->getErrorMsg());
+		}
+        $parent->set('message', JText::_('MOD_ALLICONS_POSTFLIGHT') . $this->release);
     }
 }
